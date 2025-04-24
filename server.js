339997bccca349265fc4d096ebf5874beb88cb0f
MohanAddanki <mohanaddanki25@gmail.com>
2025-04-24 17:44:25 +0530

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const path = require('file:///C:/web2/index.html');
-// ... other imports
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// ... rest of server.js
